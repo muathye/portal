e40dc0ab44632f71fe2d2bf2a413c21b74f3cd57
muath-ye <muath-ye@users.noreply.github.com>
2023-05-08 23:02:09 +0000

    deploy: 43893d227d482899bc80d20973c30b42f25e5ccf

---
+++ assets/articles.md.d6189def.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o as a,a as s}from"./app.2e2aa922.js";const m=JSON.parse('{"title":"","description":"","frontmatter":{"layout":"doc"},"headers":[],"relativePath":"articles.md"}'),i={name:"articles.md"},o=s("",12),r=[o];function n(l,c,d,p,v,h){return a(),t("div",null,r)}const u=e(i,[["render",n]]);export{m as __pageData,u as default};
