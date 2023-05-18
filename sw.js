f12df490df11238055e38071f22bd80efbce51c6
muath-ye <muath-ye@users.noreply.github.com>
2023-05-18 06:17:19 +0000

    deploy: f0015e90fa2ccafcd0605f83c2451ddc436dc978

---
+++ sw.js
@@ -0,0 +1,438 @@
+/**
+ * Copyright 2018 Google Inc. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// If the loader is already loaded, just stop.
+if (!self.define) {
+  let registry = {};
+
+  // Used for `eval` and `importScripts` where we can't get script URL by other means.
+  // In both cases, it's safe to use a global var because those functions are synchronous.
+  let nextDefineUri;
+
+  const singleRequire = (uri, parentUri) => {
+    uri = new URL(uri + ".js", parentUri).href;
+    return registry[uri] || (
+      
+        new Promise(resolve => {
+          if ("document" in self) {
+            const script = document.createElement("script");
+            script.src = uri;
+            script.onload = resolve;
+            document.head.appendChild(script);
+          } else {
+            nextDefineUri = uri;
+            importScripts(uri);
+            resolve();
+          }
+        })
+      
+      .then(() => {
+        let promise = registry[uri];
+        if (!promise) {
+          throw new Error(`Module ${uri} didn’t register its module`);
+        }
+        return promise;
+      })
+    );
+  };
+
+  self.define = (depsNames, factory) => {
+    const uri = nextDefineUri || ("document" in self ? document.currentScript.src : "") || location.href;
+    if (registry[uri]) {
+      // Module is already loading or loaded.
+      return;
+    }
+    let exports = {};
+    const require = depUri => singleRequire(depUri, uri);
+    const specialDeps = {
+      module: { uri },
+      exports,
+      require
+    };
+    registry[uri] = Promise.all(depsNames.map(
+      depName => specialDeps[depName] || require(depName)
+    )).then(deps => {
+      factory(...deps);
+      return exports;
+    });
+  };
+}
+define(['./workbox-3589c0c5'], (function (workbox) { 'use strict';
+
+  /**
+  * Welcome to your Workbox-powered service worker!
+  *
+  * You'll need to register this file in your web app.
+  * See https://goo.gl/nhQhGp
+  *
+  * The rest of the code is auto-generated. Please don't update this file
+  * directly; instead, make changes to your Workbox build configuration
+  * and re-run your build process.
+  * See https://goo.gl/2aRDsh
+  */
+
+  self.skipWaiting();
+  workbox.clientsClaim();
+  /**
+   * The precacheAndRoute() method efficiently caches and responds to
+   * requests for URLs in the manifest.
+   * See https://goo.gl/S9QRab
+   */
+
+  workbox.precacheAndRoute([{
+    "url": "404.html",
+    "revision": "371649d2b3d3f2623478e0b8730ba5c0"
+  }, {
+    "url": "android-chrome-192x192.png",
+    "revision": "8b6f412bda677ba08ec46492d2dbbb23"
+  }, {
+    "url": "android-chrome-512x512.png",
+    "revision": "951e66101f8ab8db77e9a2986a28af9a"
+  }, {
+    "url": "apple-touch-icon.png",
+    "revision": "25ab1e6e2615ec76f1febb7ecacf1ba4"
+  }, {
+    "url": "articles.html",
+    "revision": "6d43475d23061e906c83861afb8d63e4"
+  }, {
+    "url": "articles/2022-08-20-gitmoji.html",
+    "revision": "d068d7603d640265bdb989120265d526"
+  }, {
+    "url": "articles/2022-08-21-realtime-input-validation.html",
+    "revision": "97ff6de10a8bf779f6a02d8687a063e4"
+  }, {
+    "url": "articles/2022-11-24-change-default-Laravel-breeze-in-login.html",
+    "revision": "9dea90c4d16239790f062b3ac28184a1"
+  }, {
+    "url": "articles/2022-12-23-laravel-nested-eager-loading-on-polymorphic-relationships.html",
+    "revision": "9798090a6535d2b916982d0381aca1d7"
+  }, {
+    "url": "articles/2022-12-26-add-bootstrap-datepicker-in-modal.html",
+    "revision": "1099486bb48a6a70387c96ec77aad05a"
+  }, {
+    "url": "articles/2022-12-26/modal-datetime-picker.png",
+    "revision": "15769119e7cce9ba9a116fac03e9f5d6"
+  }, {
+    "url": "articles/2022-12-27-chatgpt-arabic-morning-azkar.html",
+    "revision": "51c014b389f076f0222b590c4284ed0e"
+  }, {
+    "url": "articles/2022-12-27/morning-azkar.png",
+    "revision": "6f92ec237639cc9874eef93d02fd24ec"
+  }, {
+    "url": "articles/2023-03-08-mini-self-hosted-version-chatgpt.html",
+    "revision": "aa4f9399c0a95ec21338bddde0713495"
+  }, {
+    "url": "articles/2023-03-08/mini-self-hosted-verison-chatgpt.png",
+    "revision": "a20d809a2b93470f70bed0c150f3502c"
+  }, {
+    "url": "articles/2023-03-09-from-javascript-to-mastring-nodejs.html",
+    "revision": "4e977666451585ae5d4f23544def6113"
+  }, {
+    "url": "articles/2023-04-10-add-ssh-to-bitbucket-for-windows.html",
+    "revision": "c2894eb9d22e9c8f8987010878a4e184"
+  }, {
+    "url": "articles/2023-04-17-export-vscode-extensions-list-and-download-them.html",
+    "revision": "118ad9ae4cf856c1f965a1c10e92e638"
+  }, {
+    "url": "articles/2023-04-23-how-to-use-google-maps-api-to-choose-a-location-on-your-website.html",
+    "revision": "cf50c5719a60a60aea8f9d66f4e40cce"
+  }, {
+    "url": "articles/2023-04-24/add-deploy-key.png",
+    "revision": "d3f730d15093bdf2f47998f05704b31d"
+  }, {
+    "url": "articles/2023-04-24/deploy-key.png",
+    "revision": "377de964ceb368d49736a0fe7a55e89a"
+  }, {
+    "url": "articles/2023-04-24/deploy-keys.png",
+    "revision": "930fb68e72b4cb135cab92b7d1cfca3c"
+  }, {
+    "url": "articles/2023-04-24/ssh-clone-url.png",
+    "revision": "104c5e43ba39312783c03db8f71d3e69"
+  }, {
+    "url": "articles/2023-04-25/419-page-expired.png",
+    "revision": "9d61d2526077ba165148c154ab8853a7"
+  }, {
+    "url": "articles/2023-05-17-bard-database-schema-and-implementation-in-mysql.html",
+    "revision": "6bc52ebb22ea14a0fbefb0afa1ec2b6d"
+  }, {
+    "url": "assets/app.a61e2ebf.js",
+    "revision": null
+  }, {
+    "url": "assets/articles_2022-08-20-gitmoji.md.bffed7c6.js",
+    "revision": null
+  }, {
+    "url": "assets/articles_2022-08-20-gitmoji.md.bffed7c6.lean.js",
+    "revision": null
+  }, {
+    "url": "assets/articles_2022-08-21-realtime-input-validation.md.671f2dd9.js",
+    "revision": null
+  }, {
+    "url": "assets/articles_2022-08-21-realtime-input-validation.md.671f2dd9.lean.js",
+    "revision": null
+  }, {
+    "url": "assets/articles_2022-11-24-change-default-Laravel-breeze-in-login.md.e686986d.js",
+    "revision": null
+  }, {
+    "url": "assets/articles_2022-11-24-change-default-Laravel-breeze-in-login.md.e686986d.lean.js",
+    "revision": null
+  }, {
+    "url": "assets/articles_2022-12-23-laravel-nested-eager-loading-on-polymorphic-relationships.md.a0b059f4.js",
+    "revision": null
+  }, {
+    "url": "assets/articles_2022-12-23-laravel-nested-eager-loading-on-polymorphic-relationships.md.a0b059f4.lean.js",
+    "revision": null
+  }, {
+    "url": "assets/articles_2022-12-26-add-bootstrap-datepicker-in-modal.md.a2b314a6.js",
+    "revision": null
+  }, {
+    "url": "assets/articles_2022-12-26-add-bootstrap-datepicker-in-modal.md.a2b314a6.lean.js",
+    "revision": null
+  }, {
+    "url": "assets/articles_2022-12-27-chatgpt-arabic-morning-azkar.md.853b023c.js",
+    "revision": null
+  }, {
+    "url": "assets/articles_2022-12-27-chatgpt-arabic-morning-azkar.md.853b023c.lean.js",
+    "revision": null
+  }, {
+    "url": "assets/articles_2023-03-08-mini-self-hosted-version-chatgpt.md.7986f640.js",
+    "revision": null
+  }, {
+    "url": "assets/articles_2023-03-08-mini-self-hosted-version-chatgpt.md.7986f640.lean.js",
+    "revision": null
+  }, {
+    "url": "assets/articles_2023-03-09-from-javascript-to-mastring-nodejs.md.06a95c43.js",
+    "revision": null
+  }, {
+    "url": "assets/articles_2023-03-09-from-javascript-to-mastring-nodejs.md.06a95c43.lean.js",
+    "revision": null
+  }, {
+    "url": "assets/articles_2023-04-10-add-ssh-to-bitbucket-for-windows.md.c4013232.js",
+    "revision": null
+  }, {
+    "url": "assets/articles_2023-04-10-add-ssh-to-bitbucket-for-windows.md.c4013232.lean.js",
+    "revision": null
+  }, {
+    "url": "assets/articles_2023-04-17-export-vscode-extensions-list-and-download-them.md.7e2ca668.js",
+    "revision": null
+  }, {
+    "url": "assets/articles_2023-04-17-export-vscode-extensions-list-and-download-them.md.7e2ca668.lean.js",
+    "revision": null
+  }, {
+    "url": "assets/articles_2023-04-23-how-to-use-google-maps-api-to-choose-a-location-on-your-website.md.db17af0d.js",
+    "revision": null
+  }, {
+    "url": "assets/articles_2023-04-23-how-to-use-google-maps-api-to-choose-a-location-on-your-website.md.db17af0d.lean.js",
+    "revision": null
+  }, {
+    "url": "assets/articles_2023-05-17-bard-database-schema-and-implementation-in-mysql.md.7840f7f1.js",
+    "revision": null
+  }, {
+    "url": "assets/articles_2023-05-17-bard-database-schema-and-implementation-in-mysql.md.7840f7f1.lean.js",
+    "revision": null
+  }, {
+    "url": "assets/articles.md.e5e86c57.js",
+    "revision": null
+  }, {
+    "url": "assets/articles.md.e5e86c57.lean.js",
+    "revision": null
+  }, {
+    "url": "assets/en_index.md.d4c2802c.js",
+    "revision": null
+  }, {
+    "url": "assets/en_index.md.d4c2802c.lean.js",
+    "revision": null
+  }, {
+    "url": "assets/index.md.300743d6.js",
+    "revision": null
+  }, {
+    "url": "assets/index.md.300743d6.lean.js",
+    "revision": null
+  }, {
+    "url": "assets/inter-cyrillic-ext.0877b0d9.woff2",
+    "revision": null
+  }, {
+    "url": "assets/inter-cyrillic.f8750142.woff2",
+    "revision": null
+  }, {
+    "url": "assets/inter-greek-ext.3e6f6728.woff2",
+    "revision": null
+  }, {
+    "url": "assets/inter-greek.117e1956.woff2",
+    "revision": null
+  }, {
+    "url": "assets/inter-italic-cyrillic-ext.33bd5a8e.woff2",
+    "revision": null
+  }, {
+    "url": "assets/inter-italic-cyrillic.ea42a392.woff2",
+    "revision": null
+  }, {
+    "url": "assets/inter-italic-greek-ext.4fbe9427.woff2",
+    "revision": null
+  }, {
+    "url": "assets/inter-italic-greek.8f4463c4.woff2",
+    "revision": null
+  }, {
+    "url": "assets/inter-italic-latin-ext.bd8920cc.woff2",
+    "revision": null
+  }, {
+    "url": "assets/inter-italic-latin.bd3b6f56.woff2",
+    "revision": null
+  }, {
+    "url": "assets/inter-italic-vietnamese.6ce511fb.woff2",
+    "revision": null
+  }, {
+    "url": "assets/inter-latin-ext.7cc429bc.woff2",
+    "revision": null
+  }, {
+    "url": "assets/inter-latin.4fe6132f.woff2",
+    "revision": null
+  }, {
+    "url": "assets/inter-roman-cyrillic-ext.e75737ce.woff2",
+    "revision": null
+  }, {
+    "url": "assets/inter-roman-cyrillic.5f2c6c8c.woff2",
+    "revision": null
+  }, {
+    "url": "assets/inter-roman-greek-ext.ab0619bc.woff2",
+    "revision": null
+  }, {
+    "url": "assets/inter-roman-greek.d5a6d92a.woff2",
+    "revision": null
+  }, {
+    "url": "assets/inter-roman-latin-ext.0030eebd.woff2",
+    "revision": null
+  }, {
+    "url": "assets/inter-roman-latin.2ed14f66.woff2",
+    "revision": null
+  }, {
+    "url": "assets/inter-roman-vietnamese.14ce25a6.woff2",
+    "revision": null
+  }, {
+    "url": "assets/inter-vietnamese.2c644a25.woff2",
+    "revision": null
+  }, {
+    "url": "assets/projects.md.fc84db5e.js",
+    "revision": null
+  }, {
+    "url": "assets/projects.md.fc84db5e.lean.js",
+    "revision": null
+  }, {
+    "url": "assets/resume.md.57aae160.js",
+    "revision": null
+  }, {
+    "url": "assets/resume.md.57aae160.lean.js",
+    "revision": null
+  }, {
+    "url": "assets/snippets_2023-05-16-retrieve-related-tables-of-a-specific-table-in-mysql.md.1012e42c.js",
+    "revision": null
+  }, {
+    "url": "assets/snippets_2023-05-16-retrieve-related-tables-of-a-specific-table-in-mysql.md.1012e42c.lean.js",
+    "revision": null
+  }, {
+    "url": "assets/snippets.md.2385124a.js",
+    "revision": null
+  }, {
+    "url": "assets/snippets.md.2385124a.lean.js",
+    "revision": null
+  }, {
+    "url": "assets/style.2da44ed4.css",
+    "revision": null
+  }, {
+    "url": "en/index.html",
+    "revision": "23606c5f91c62bbcbaa3ab5fa399baa9"
+  }, {
+    "url": "favicon-16x16.png",
+    "revision": "d4de349fd29d46f50aeac6cb294ae991"
+  }, {
+    "url": "favicon-32x32.png",
+    "revision": "8a8623ddf50c485ce105569baa5c2afd"
+  }, {
+    "url": "favicon.ico",
+    "revision": "d3091d363d8b52c41c1f227d8105dfaf"
+  }, {
+    "url": "images/background/docs-dark@tinypng.1bbe175e.png",
+    "revision": null
+  }, {
+    "url": "images/muathye.png",
+    "revision": "da6a18db9204b5d051aaf7e225105b8b"
+  }, {
+    "url": "images/muathye.svg",
+    "revision": "8b8e61771570c80c5470b1adaf2c44ee"
+  }, {
+    "url": "index.html",
+    "revision": "ff6a92b4d08b7b33e2276fa59772c683"
+  }, {
+    "url": "masked-icon.svg",
+    "revision": "8b8e61771570c80c5470b1adaf2c44ee"
+  }, {
+    "url": "mstile-150x150.png",
+    "revision": "f020256b2109323117574ed955e15b13"
+  }, {
+    "url": "projects.html",
+    "revision": "431f8feaf84b08d96ddbc0f152a52256"
+  }, {
+    "url": "projects/eazycare.app/new-logo.svg",
+    "revision": "8bcc0aef753052adae872ffae4e45ac4"
+  }, {
+    "url": "projects/mokasweets.com/mokasweets-logo.svg",
+    "revision": "c70f81c8d6ac2f8a1b670ef749cfb820"
+  }, {
+    "url": "projects/OpenSale/open-sale.svg",
+    "revision": "a7590fa654f4499d1d36ad147630556d"
+  }, {
+    "url": "projects/sdelivery.co/dark.png",
+    "revision": "47b561ffb45d803e592be20b255f7b00"
+  }, {
+    "url": "projects/sdelivery.co/light.png",
+    "revision": "da1ff181df734f37f8e3c13dbab91dbc"
+  }, {
+    "url": "pwa-192x192.png",
+    "revision": "8b6f412bda677ba08ec46492d2dbbb23"
+  }, {
+    "url": "pwa-512x512.png",
+    "revision": "951e66101f8ab8db77e9a2986a28af9a"
+  }, {
+    "url": "registerSW.js",
+    "revision": "1872c500de691dce40960bb85481de07"
+  }, {
+    "url": "resume.html",
+    "revision": "35cc3a2f6aa8585bf0f313296b6da262"
+  }, {
+    "url": "robots.txt",
+    "revision": "5e0bd1c281a62a380d7a948085bfe2d1"
+  }, {
+    "url": "safari-pinned-tab.svg",
+    "revision": "896ff3300aa31eae3864243b6f116f66"
+  }, {
+    "url": "snippets.html",
+    "revision": "93f62174d689c2cea7853d134d162d71"
+  }, {
+    "url": "snippets/2023-05-16-retrieve-related-tables-of-a-specific-table-in-mysql.html",
+    "revision": "13e57199ba8bae280772e82c1b3d6b3d"
+  }, {
+    "url": "snippets/2023-05-16/retrieve-related-tables-of-a-specific-table-in-mysql.png",
+    "revision": "ba013ed07b09fbf3c05eb1aa158c5a3f"
+  }, {
+    "url": "pwa-192x192.png",
+    "revision": "8b6f412bda677ba08ec46492d2dbbb23"
+  }, {
+    "url": "pwa-512x512.png",
+    "revision": "951e66101f8ab8db77e9a2986a28af9a"
+  }, {
+    "url": "manifest.webmanifest",
+    "revision": "b1eec4280dc7c7f0bca6689acb26fb4b"
+  }], {});
+  workbox.cleanupOutdatedCaches();
+  workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html")));
+
+}));
