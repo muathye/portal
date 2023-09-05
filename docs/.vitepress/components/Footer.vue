cace77335c193e591093af6e9958e5a745f01ce5
muathye <muath.ye@gmail.com>
2023-09-06 00:27:08 +0300

    wip: test process.env.APP_URL

--- docs/.vitepress/components/Footer.vue
+++ docs/.vitepress/components/Footer.vue
@@ -2,12 +2,13 @@
 import { ref } from 'vue'
 
 const currentYear = ref(new Date().getFullYear())
+const hostname = process.env.APP_URL
 
 </script>
 <template>
     <footer data-v-80147a0d="" data-v-8f7cd5e4="" class="VPFooter">
         <div data-v-80147a0d="" class="container"><!--v-if-->
-            <p data-v-80147a0d="" class="copyright">Copyright © 2022-{{currentYear}} | Muath Alsowadi</p>
+            <p data-v-80147a0d="" class="copyright">Copyright © 2022-{{ currentYear }} | <a href="{{hostname}}">Muath Alsowadi</a></p>
         </div>
     </footer>
 </template>
