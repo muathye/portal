2541d8d8fe6a4d6192d9bc7cf87edb960fc621fb
muathye <muath.ye@gmail.com>
2023-10-19 10:31:47 +0300

    chore: change the title of website on rss

--- docs/.vitepress/GenerateRSS.ts
+++ docs/.vitepress/GenerateRSS.ts
@@ -8,7 +8,7 @@ const currentYear = new Date().getFullYear()
 
 export async function GenerateRSS(config: SiteConfig) {
   const feed = new Feed({
-    title: 'Muath Alsowadi - Web designer and developer',
+    title: 'Muath Alsowadi',
     description: 'Founder of @open-sale. Co-founder of @YemenOpenSource. Passionate and highly skilled senior full-stack web developer proficient in HTML, CSS, JavaScript, Vue.js, PHP, SQL, Laravel, and API development, with a strong commitment to leveraging technology to create innovative solutions and positively impact the world.',
     id: hostname,
     link: hostname,
